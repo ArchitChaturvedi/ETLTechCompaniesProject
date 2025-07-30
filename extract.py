e37018b0277744ce54a550b0569642281d0c3f7c
ArchitChaturvedi <archie.4ved@gmail.com>
2025-07-30 01:06:14 -0700

    Update extract.py

--- extract.py
+++ extract.py
@@ -5,15 +5,7 @@ import os
 logger = logging.getLogger(__name__)
 
 def extract_data(file_name: str, **kwargs):
-    """
-    Extracts data from a CSV file located in the /data directory.
-
-    Args:
-        file_name (str): Name of the CSV file to extract (e.g., 'tech_companies_2024.csv').
-
-    Returns:
-        pd.DataFrame: Extracted raw data as a DataFrame.
-    """
+    
     data_path = os.path.join('/opt/airflow/data', file_name)
 
     try:

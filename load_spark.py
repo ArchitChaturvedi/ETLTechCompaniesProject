1b00057fe9bc58473899833d0b57fb9861c3b623
ArchitChaturvedi <archie.4ved@gmail.com>
2025-07-30 13:43:25 -0700

    Update load_spark.py

--- load_spark.py
+++ load_spark.py
@@ -8,7 +8,6 @@ logging.basicConfig(level=logging.INFO)
 
 def load_data_spark(file_name: str, **kwargs):
     try:
-        # Load DB config
         config_path = os.path.join('/opt/airflow/config', 'db_config.yaml')
         with open(config_path, 'r') as file:
             config = yaml.safe_load(file)
@@ -16,16 +15,13 @@ def load_data_spark(file_name: str, **kwargs):
         db_url = config['neon']['connection_string']
         table_name = os.path.splitext(file_name)[0]
 
-        # Initialize Spark session
         spark = SparkSession.builder \
             .appName("DataLoading") \
             .getOrCreate()
 
-        # Read CSV into Spark DataFrame
         data_path = os.path.join('/opt/airflow/data', file_name)
         df = spark.read.option("header", "true").csv(data_path)
 
-        # Write to PostgreSQL using JDBC
         df.write \
             .format("jdbc") \
             .option("url", db_url) \
